d729eee0e8f22c07cdae49b4f5cd8364e55283b9
Jb Laurans <monkeyshould@gmail.com>
2021-01-22 01:34:33 +0100

    Mise en page Texte

--- lib/screens/guest/Auth.dart
+++ lib/screens/guest/Auth.dart
@@ -10,7 +10,47 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                text: TextSpan(
+                  text: 'Everyone has \n'. toUpperCase(),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30.0
+                  ),
+                  children: [
+                    TextSpan(
+                      text:'knowledge\n'.toUpperCase(), 
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                    TextSpan(
+                      text:'to share'.toUpperCase(),     
+                    ),
+                  ],
+                )
+              ),
+              Text(
+                'It all starts here',
+                style:TextStyle(
+                  fontStyle: FontStyle.italic)
+                ),
+              SizedBox(height: 50.0,
+              ),
+              Text('Enter your email'),
+              
+
+            ],
+          ),
+        ),
+      ),
     
     );
   }
